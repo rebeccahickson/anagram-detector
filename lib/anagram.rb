b55c1e1a8e8659d14818f2d2d9a98bb08232ab7b
Rebecca Hickson <rebecca.l.hickson@gmail.com>
2020-10-29 15:37:18 -0400

    commit tree 843c97c0dce15deacf04e4437fc2b44897e5f0fb

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(test_words)
+    test_words.select { |test_word| @word.split('').sort == test_word.split('').sort }
+  end
+end
